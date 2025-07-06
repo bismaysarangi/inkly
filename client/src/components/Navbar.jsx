3edae9a8b4efa7639ffde435eafabc0a3c240439
Bismay <bismaysarangi@gmail.com>
2025-07-06 21:56:32 +0530

    style: refactor Navbar component styling and define navbar class in CSS

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="z-10 sticky top-0 flex items-center gap-12 w-full px-[5vw] py-5 h-[80px] border-b border-grey">
+    <nav className="navbar">
       <Link to="/" className="flex-none w-10">
         <img src="/small-logo.png" alt="logo" className="w-full" />
       </Link>
