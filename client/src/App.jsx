15001f40274d6f3f78a0fcc7965737dc7762bba2
Bismay <bismaysarangi@gmail.com>
2025-07-10 23:25:37 +0530

    feat: implement UserAuthForm component and update routing in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
+import UserAuthForm from "./pages/UserAuthForm";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
-        <Route path="/signin" element={<h1>Sign In Page</h1>} />
-        <Route path="/signup" element={<h1>Sign Up page</h1>} />
+        <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+        <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
       </Route>
     </Routes>
   );
