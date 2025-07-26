27e392da21b3c99e6ea9b938d2461d291bac40a5
Bismay <bismaysarangi@gmail.com>
2025-07-26 19:33:00 +0530

    feat(error): implement centralized error handling and update signup controller to use error handler

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,12 @@
 import User from '../models/user-model.js';
 import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     
     if (!username || !email || !password || username.trim() === '' || email.trim() === '' || password.trim() === '') {
-        return res.status(400).json({ message: 'All fields are required' });
+        return next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -18,10 +19,9 @@ export const signup = async (req, res) => {
 
     try {
         await newUser.save();
+        res.json('signup successful');
     } catch (error) {
-        return res.status(500).json({ message: 'Error saving user', error });
+        return next({ statusCode: 500, message: 'Error saving user', error });
     }
-
-    await newUser.save();
-    res.json('signup successful');
+    
 }
--- server/server.js
+++ server/server.js
@@ -19,3 +19,9 @@ app.listen(3000, () => {
 app.use(express.json());
 app.use('/server/user', userRoute);
 app.use('/server/auth', authRoute);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({ message });
+});
