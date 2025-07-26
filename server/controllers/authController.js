bf5316218e5ff83204a2cfcbc5be698e5f1573b0
Bismay <bismaysarangi@gmail.com>
2025-07-26 19:22:39 +0530

    fix: added hashed password for better security

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from '../models/user-model.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
@@ -7,10 +8,12 @@ export const signup = async (req, res) => {
         return res.status(400).json({ message: 'All fields are required' });
     }
 
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
     const newUser = new User ({
         username,
         email,
-        password
+        password: hashedPassword
     });
 
     try {
