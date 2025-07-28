0c59a7d03270e70fe933701ee2657990c10ba358
Bismay <bismaysarangi@gmail.com>
2025-07-28 23:06:26 +0530

    feat(footer): add Footer component with navigation links and social media icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
       </Routes>
+      <Footer />
     </>
   );
 };
