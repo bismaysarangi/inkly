ef1a0d58e0b6c1c05a26f3df6ae17f90f4da6304
Bismay <bismaysarangi@gmail.com>
2025-08-03 20:57:29 +0530

    feat(dashboard): implement tab handling and sidebar structure in Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log("Tab from URL:", tabFromUrl);
+  }, [location.search]);
+  return (
+    <div>
+      <div>{/* Sidebar */}</div>
+      {/* Sections */}
+    </div>
+  );
 };
 
 export default Dashboard;
